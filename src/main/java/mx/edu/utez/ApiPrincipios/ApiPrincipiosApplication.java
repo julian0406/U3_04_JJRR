cbef449c5ce744f177727105d1c3f42770432589
julian0406 <20223tn075@utez.edu.mx>
2025-06-16 17:45:16 -0600

    [FIRST] Se suben los documentos

---
+++ src/main/java/mx/edu/utez/ApiPrincipios/ApiPrincipiosApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.ApiPrincipios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPrincipiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPrincipiosApplication.class, args);
+	}
+
+}
